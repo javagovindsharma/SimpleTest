23fe3dfccd48a3abc51075c354190054c8b86d50
Govind Sharma <37431318+javagovindsharma@users.noreply.github.com>
2019-06-10 13:04:16 +0530

    Create FullScreenCaptureExample.java
    screenCaputure

---
+++ FullScreenCaptureExample.java
@@ -0,0 +1,28 @@
+package com.fs.screc.ScreenRecording;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+ 
+public class FullScreenCaptureExample {
+ 
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+ 
+            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+            File file = new File("screen-capture.png");
+            boolean status = ImageIO.write(bufferedImage, "png", file);
+            System.out.println("Screen Captured ? " + status + " File Path:- " + file.getAbsolutePath());
+ 
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
